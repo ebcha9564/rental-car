c42605b1b4d3231902f4193aa82dc1b102d5b134
Eunbin Cha <eric9564@gmail.com>
2025-07-03 18:29:08 +0900

    Working rental car homepage
    Working rental car homepage

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { useState } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import CarDetails from './pages/CarDetails'
+import MyBookings from './pages/MyBookings'
+import Cars from './pages/Cars'
+
+const App = () => {
+  const [showLogin, setShowLogin] = useState(false)
+  const isOwnerPath = useLocation().pathname.startsWith('/owner')
+
+  return (
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/car-details/:id' element={<CarDetails/>}/>
+        <Route path='/cars' element={<Cars/>}/>
+        <Route path='/my-bookings' element={<MyBookings/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
